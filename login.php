dd7c6bacaec7e03dc469c9a22075d4f777157925
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-02-12 00:05:08 +0200

    Updated header
    Updated header, but not finished.

--- login.php
+++ login.php
@@ -38,6 +38,20 @@
                     <font color="white"><h2 class="text-right" color="white">Aleksey Zhuk [ 1 ]</h2></font>
                 </div>
             </div>
+            <nav class="navbar navbar-dark bg-dark">
+                <a class="navbar-brand" href="index.php">Home</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     </div>
     <div class="container">
