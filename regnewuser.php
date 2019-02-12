53546682b102d6ce1abd1061597b204d98ccb25d
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-02-12 13:07:20 +0200

    Updated connection to DB
    Updated connection to DB

--- regnewuser.php
+++ regnewuser.php
@@ -1,4 +1,7 @@
 <?php
+    include 'engine.php';
+
+    /* New User Registration */
     if(isset($_POST['FirstName']) && isset($_POST['SecondName']) && isset($_POST['Login']) && isset($_POST['Email']) && isset($_POST['Password'])){
         $FirstName = $_POST['FirstName'];
         $SecondName = $_POST['SecondName'];
@@ -8,8 +11,8 @@
         $RePassword = $_POST['RePassword'];
 
         if($Password == $RePassword){
-            $Password_MD5 = MD5($Password);
-            $pdo = new PDO("mysql:host=localhost;dbname=secondmodule","root","");
+            $Password_MD5 = MD5($Password); // Convert Password to MD5 hash;
+            $pdo = new PDO($MySQL_Path, $DataBaseLogin, $DataBasePass); // Connection to Data Base;
             $send_data = $pdo -> query("INSERT INTO users (Id, firstname, secondname, login, email, password, userphoto) VALUES (NULL, '$FirstName', '$SecondName', '$Login', '$Email', '$Password_MD5', '')");
             echo "Congratulations, you have registered";
         }
