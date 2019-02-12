54ef3517e92e7fd937134aa2702a64dea5693e81
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-02-12 13:04:11 +0200

    Created file for logic
    Created file for logic

---
+++ engine.php
@@ -0,0 +1,9 @@
+<?php
+    /* This file created for creation and declaration varriables and functions */
+
+    /* Data Base Configuration */
+    $MySQL_Path = "mysql:host=localhost;dbname=secondmodule";
+    $DataBaseLogin = "root";
+    $DataBasePass ="";
+
+?>
