a8699aa4570cfcc6326bd152bed2fa48874f563c
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-02-13 00:59:32 +0200

    Created user updating
    Created user updating in Data Base by user Id.

---
+++ user_update.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();
+    include "engine.php";
+    $User_ID = $_SESSION['userid']; // Retreiweing User Id;
+    $FirstName = $_POST['First_Name']; // Retreiweing Updated First_Name;
+    $SecondName = $_POST['Second_Name']; //Retreiweing Updated Second_Name;
+    $Email = $_POST['Email']; // Retreiweing Updated email address;
+    $pdo = new PDO($MySQL_Path, $DataBaseLogin, $DataBasePass); // Connection to Data Base;
+    $statement = $pdo -> query("UPDATE `users` SET `firstname`='$FirstName',`secondname`='$SecondName', `secondname`='$SecondName' WHERE Id='$User_ID'"); // Updating;
+    header('Location:profile.php');
+?>
