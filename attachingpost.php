b0fd17d3a17e9c74da4dc5a7af7b4dbb2148aa44
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-02-15 22:23:49 +0200

    Sending post details to Data Base.
    I have wrote code which process arrays and insert to data base.

--- attachingpost.php
+++ attachingpost.php
@@ -1,8 +1,31 @@
 <?php
     session_start();
     include 'engine.php';
-    $pdo = new PDO($MySQL_Path, $DataBaseLogin, $DataBasePass); // Connection to Data Base;
-    $send_data = $pdo -> query("INSERT INTO posts (Id, post_name, post_description, post_text, author_id, post_image) VALUES (NULL, '$FirstName', '$SecondName', '$Login', '$Email', '$Password_MD5', '')");
 
-    var_dump($_POST, $_FILES);
+    /* Post details */
+    /* Start */
+    $post_name = $_POST['Post_Name']; // Take post name and assign to varriable;
+    $post_description = $_POST['Post_Description']; // Take post description and assign to varriable;
+    $post_text = $_POST['Post_Text']; // Take post text and assign to varriable;
+    $author_id = $_SESSION['userid']; // Take user id and assign to varriable;
+    $post_image = $_FILES['name'];
+    /* Post details */
+    /* End */
+
+    /* Move post image to local storage */
+    /* Start */
+    if(isset($post_image)){
+        $OriginalPhotoName = basename($_FILES['post_photo']['name']); // Take full filename with extension; 
+        move_uploaded_file($_FILES['post_photo']['tmp_name'], $PathFiles.$OriginalPhotoName); // Move file to photos folder;    
+    }else{
+        $post_image = $DefaultPhotoName; // if owner leave default photo, put dafault photo name;
+    }
+    /* Move post image to local storage */
+    /* End */
+
+    $pdo = new PDO($MySQL_Path, $DataBaseLogin, $DataBasePass); // Connection to Data Base;
+    $send_data = $pdo -> query("INSERT INTO posts (post_id, post_name, post_description, post_text, author_id, post_image) VALUES (NULL, '$post_name', '$post_description', '$post_text', '$author_id', '$post_image')");
+    header('Location:index.php'); // Once all details been sent, move user to main page;
+    //var_dump($_POST, $_FILES, $_SESSION);
+    //var_dump($post_image);
 ?>
