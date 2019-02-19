8ba076b5f8f8b0906c17350558a65b2ba5a74e97
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-02-19 18:18:51 +0200

    Updated DB.
    Updated DB.

--- secondmodule.sql
+++ secondmodule.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Час створення: Лют 19 2019 р., 19:01
+-- Час створення: Лют 19 2019 р., 19:17
 -- Версія сервера: 8.0.12
 -- Версія PHP: 7.2.10
 
@@ -45,8 +45,8 @@ INSERT INTO `posts` (`post_id`, `post_name`, `post_description`, `post_text`, `a
 (1, 'Hello everyone!', 'Every one should know what he should to do for effective learing PHP.', 'PHP is a general-purpose scripting language primarily used in web development to enhance web pages. First created in 1994 by Rasmus Lerdorf, PHP began life as a suite of scripts known as “Personal Home Page Tools.” In 1995, Rasmus expanded his suite to add functionality and released the source code to the public.\r\n\r\nUnlike JavaScript, which is client-side, PHP is server-side. When the browser requests information from the server, the server executes the code and sends the result to the client. Typically, the output is an HTML file, but this can also include CSS and JavaScript. The browser uses this information to create the web page.', 1, 'reinis-birznieks-1355047-unsplash.jpg'),
 (2, 'Learn the Basics of PHP and start create soft.', 'Few effective things for learning PHP programming language.', 'Learn PHP online from the best PHP tutorials & courses recommended by the programming community.', 2, 'reinis-birznieks-1355047-unsplash.jpg'),
 (3, 'What about try matrix with php?', 'One of the best method receive practive in programin language.', 'Hello everyone. A lot of users asking - how we can get practice during learning php pragramming language. The main things - everyday write code.', 8, 'reinis-birznieks-1355047-unsplash.jpg'),
-(4, 'Reading more books.', 'Today people read books less than 10 years ago.', 'Books lost their pupular in the world, but they still stay the best way in personal upgrading.', 2, 'tesla-model-s-tesla-model-s-633.jpg'),
-(5, 'PHP Training.', 'PHP is the world’s most popular server-side scripting language.', 'f you’ve been learning front-end development and want to take the next step, PHP is an ideal place to start. Although you have the choice of many other server-side technologies like Rails and Node, PHP is especially suited to a beginner. This is because it’s the easiest to get up and running and it plugs right into HTML.\r\n\r\nHere are 6 of Code Conquest’s top PHP training recommendations. There’s something from every category – free and premium, online and offline, basic and extensive, hands-on and theoretical. Guaranteed, one is the perfect training for you.', 8, 'matrix-356024.jpg'),
+(4, 'Reading more books.', 'Today people read books less than 10 years ago.', 'Books lost their pupular in the world, but they still stay the best way in personal upgrading.', 1, 'tesla-model-s-tesla-model-s-633.jpg'),
+(5, 'PHP Training.', 'PHP is the world’s most popular server-side scripting language.', 'f you’ve been learning front-end development and want to take the next step, PHP is an ideal place to start. Although you have the choice of many other server-side technologies like Rails and Node, PHP is especially suited to a beginner. This is because it’s the easiest to get up and running and it plugs right into HTML.\r\n\r\nHere are 6 of Code Conquest’s top PHP training recommendations. There’s something from every category – free and premium, online and offline, basic and extensive, hands-on and theoretical. Guaranteed, one is the perfect training for you.', 2, 'matrix-356024.jpg'),
 (6, 'Knowledge Center.', 'Whether you want to learn how to make a website.', 'Get help with your coding or even start a career, the Code Conquest knowledge center has all the info you need.\r\n\r\nYou can learn how to start working on your own self-directed coding project or make a website. You’ll get the opportunity to familiarize yourself with a range of different coding tools, like GitHub and text editors. You can also find out where to get coding help.\r\n\r\nThis is also the place to introduce yourself to some advanced concepts, once you’re confident you’ve mastered the basics of coding. Finally, there are tips for finding a job as a coder or starting a business.', 9, 'nebo-tuchi-molniia-stikhiia.jpg');
 
 -- --------------------------------------------------------
@@ -72,7 +72,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`Id`, `firstname`, `secondname`, `login`, `email`, `password`, `userphoto`) VALUES
 (1, 'Aleksey', 'Zhuk', 'Aleksey', 'aleksey@mail.com', '722C53076934353B6179953B622A4FA9', ''),
 (2, 'Dima', 'Zhuk', 'Dimka', 'dimka@mail.com', '489d460f0a77910a5d019701859d50d7', 'titanic_647_041416113640.jpg'),
-(8, 'Lena', 'Zhuk', 'Lenchik', 'lena@mail.com', '3616A35765CDD0B13B162BDCA17374AA', ''),
+(8, 'Lena', 'Zhuk', 'Lenchik', 'lena@mail.com', '73586c09cf0d2e898aaa75d456f4e044', ''),
 (9, 'Denis', 'Zhuk', 'Denchik', 'denchik@mail.com', 'A3B8EB6FAAF230BD0944847C527D30BF', 'denis.png');
 
 --
