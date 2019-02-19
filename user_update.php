abdd13dd0855c802bd1a46ca8568531400737f78
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-02-19 15:24:38 +0200

    Fixed error in updating user data.
    Error: missed email records. =)

--- user_update.php
+++ user_update.php
@@ -6,6 +6,6 @@
     $SecondName = $_POST['Second_Name']; //Retreiweing Updated Second_Name;
     $Email = $_POST['Email']; // Retreiweing Updated email address;
     $pdo = new PDO($MySQL_Path, $DataBaseLogin, $DataBasePass); // Connection to Data Base;
-    $statement = $pdo -> query("UPDATE `users` SET `firstname`='$FirstName',`secondname`='$SecondName', `secondname`='$SecondName' WHERE Id='$User_ID'"); // Updating;
+    $statement = $pdo -> query("UPDATE `users` SET `firstname`='$FirstName',`secondname`='$SecondName', `secondname`='$SecondName', `email`='$Email'  WHERE Id='$User_ID'"); // Updating;
     header('Location:profile.php');
 ?>
